fd9caa1b5ddba867d805cb300148684809ed997a
Ryan Puskas <ryanpuskas@gmail.com>
2015-05-23 18:53:34 -0700

    Adds nhibernate and simple mappings

--- Domain.cs
+++ Domain.cs
@@ -1,25 +1,60 @@
-﻿namespace AggregatePatterns
+﻿using FluentNHibernate.Mapping;
+
+namespace AggregatePatterns
 {
     public class Match
     {
-        public Trade Trade;
-
-        public Clearance Clearance;
+        public int Id { get; set; }
+        
+        public Trade Trade { get; set; }
+        
+        public Clearance Clearance { get; set; }
 
         public Match(Trade trade, Clearance clearance)
         {
-            
+            Trade = trade;
+            Clearance = clearance;
         }
     }
 
     public class Trade
     {
-        public int Id;
+        public int Id { get; set; }
+        public decimal Amount;
     }
 
     public class Clearance
     {
-        public int Id;
+        public int Id { get; set; }
+        public decimal Amount;
+    }
+
+    public class MatchMap : ClassMap<Match>
+    {
+        public MatchMap()
+        {
+            Id(x => x.Id);
+            References(x => x.Trade);
+            References(x => x.Clearance);
+        }
+    }
+
+    public class TradeMap : ClassMap<Trade>
+    {
+        public TradeMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Amount);
+        }
+    }
+
+    public class ClearanceMap : ClassMap<Clearance>
+    {
+        public ClearanceMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Amount);
+        }
     }
 
 }
