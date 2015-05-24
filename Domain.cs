e0272dbd1dbfa27c50e396127ee5e8da94c80b6b
Ryan Puskas <ryanpuskas@gmail.com>
2015-05-24 05:52:10 -0700

    Adds in memory database test

--- Domain.cs
+++ Domain.cs
@@ -1,42 +1,57 @@
-﻿using FluentNHibernate.Cfg;
+﻿using System.Linq;
+using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using FluentNHibernate.Mapping;
 using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Linq;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
 
 namespace AggregatePatterns
 {
-    class TestFixture
+    [TestFixture]
+    public class MyTestFixture
     {
-        public void Test()
-        {
-            var sessionFactory = CreateSessionFactory();
-            using (var session = sessionFactory.OpenSession())
-            {
-                using (var transaction = session.BeginTransaction())
-                {
-                    var customer = new Trade { Amount = 123 };
-                    session.SaveOrUpdate(customer);
-                    transaction.Commit();
-                }
-            }
-        }
+        private static Configuration _configuration;
 
         private static ISessionFactory CreateSessionFactory()
         {
             return Fluently.Configure()
                 .Database(SQLiteConfiguration.Standard.InMemory)
-                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<TestFixture>())
+                .ExposeConfiguration(c => _configuration = c)
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<MyTestFixture>())
                 .BuildSessionFactory();
+        }
+
+        [Test]
+        public void Test()
+        {
+            var sessionFactory = CreateSessionFactory();
+            var schemaExport = new SchemaExport(_configuration);
+            using (var session = sessionFactory.OpenSession())
+            {
+                schemaExport.Execute(true, true, false, session.Connection, null);
+                var customer = new Trade { Amount = 123 };
+                session.SaveOrUpdate(customer);
+
+                session.Flush();
+
+                var foo = session.Query<Trade>().ToList();
+                Assert.AreEqual(foo.First().Amount, 123);
+            }
         }
     }
 
     public class Match
     {
-        public int Id { get; set; }
+        public virtual int Id { get; set; }
         
-        public Trade Trade { get; set; }
+        public virtual Trade Trade { get; set; }
         
-        public Clearance Clearance { get; set; }
+        public virtual Clearance Clearance { get; set; }
+
+        protected Match() { }
 
         public Match(Trade trade, Clearance clearance)
         {
@@ -47,14 +62,14 @@ namespace AggregatePatterns
 
     public class Trade
     {
-        public int Id { get; set; }
-        public decimal Amount;
+        public virtual int Id { get; set; }
+        public virtual decimal Amount { get; set; }
     }
 
     public class Clearance
     {
-        public int Id { get; set; }
-        public decimal Amount;
+        public virtual int Id { get; set; }
+        public virtual decimal Amount { get; set; }
     }
 
     public class MatchMap : ClassMap<Match>
