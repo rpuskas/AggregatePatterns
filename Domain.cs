bb0e64f1f36c0dd0401a8ee62abf8251b2414658
Ryan Puskas <ryanpuskas@gmail.com>
2015-05-23 18:30:12 -0700

    Creates domain model

---
+++ Domain.cs
@@ -0,0 +1,25 @@
+﻿namespace AggregatePatterns
+{
+    public class Match
+    {
+        public Trade Trade;
+
+        public Clearance Clearance;
+
+        public Match(Trade trade, Clearance clearance)
+        {
+            
+        }
+    }
+
+    public class Trade
+    {
+        public int Id;
+    }
+
+    public class Clearance
+    {
+        public int Id;
+    }
+
+}
