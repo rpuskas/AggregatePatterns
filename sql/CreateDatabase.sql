26db8eab5861154bc121af60f8d3f9fa2dbae728
Ryan Puskas <ryanpuskas@gmail.com>
2015-07-07 08:04:58 -0700

    Adds database creation script

---
+++ sql/CreateDatabase.sql
@@ -0,0 +1,43 @@
+USE [master]
+GO
+
+CREATE DATABASE AggregatePatterns3
+GO
+
+USE AggregatePatterns3
+
+CREATE TABLE Clearance(
+	Id [bigint] IDENTITY(1,1) NOT NULL,
+	Amount [money] NULL,
+	CONSTRAINT [PK_Clearance] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
+GO 
+
+CREATE TABLE Trade(
+	Id [bigint] IDENTITY(1,1) NOT NULL,
+	Amount [money] NULL,
+	CONSTRAINT [PK_Trade] PRIMARY KEY CLUSTERED ([Id] ASC)
+) 
+GO
+
+CREATE TABLE Match(
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Clearance_Id] [bigint] NOT NULL,
+	[Trade_Id] [bigint] NOT NULL,
+	CONSTRAINT [PK_Match] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
+GO
+
+ALTER TABLE Match  WITH CHECK ADD  CONSTRAINT [FK_Match_Clearance1] FOREIGN KEY([Clearance_Id])
+REFERENCES Clearance ([Id])
+GO
+
+ALTER TABLE Match CHECK CONSTRAINT [FK_Match_Clearance1]
+GO
+
+ALTER TABLE Match  WITH CHECK ADD  CONSTRAINT [FK_Match_Trade1] FOREIGN KEY([Trade_Id])
+REFERENCES Trade ([Id])
+GO
+
+ALTER TABLE Match CHECK CONSTRAINT [FK_Match_Trade1]
+GO
