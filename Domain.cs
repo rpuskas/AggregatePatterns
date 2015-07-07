f3d4b352ccc15f1fb1803922056603cf89604d0a
Ryan Puskas <ryanpuskas@gmail.com>
2015-07-07 07:51:20 -0700

    Adds Dapper comparison to NHibernate

--- Domain.cs
+++ Domain.cs
@@ -1,44 +1,123 @@
-﻿using System.Linq;
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using Dapper;
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using FluentNHibernate.Mapping;
+using Newtonsoft.Json;
 using NHibernate;
-using NHibernate.Cfg;
 using NHibernate.Linq;
-using NHibernate.Tool.hbm2ddl;
 using NUnit.Framework;
 
 namespace AggregatePatterns
 {
+
+    // Dapper: 19280 to 48620 = 29MB @ .88 seconds
+    // NHibernate: 59472 to 270316 = 210MB @ 5 seconds
+
     [TestFixture]
     public class MyTestFixture
     {
-        private static Configuration _configuration;
-
         private static ISessionFactory CreateSessionFactory()
         {
             return Fluently.Configure()
-                .Database(SQLiteConfiguration.Standard.InMemory)
-                .ExposeConfiguration(c => _configuration = c)
+                .Database(MsSqlConfiguration.MsSql2008.ConnectionString(
+                @"Server=TW-WINDOWS\SQLEXPRESS;Database=AggregatePatterns;Trusted_Connection=True;"))
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<MyTestFixture>())
                 .BuildSessionFactory();
         }
 
         [Test]
-        public void Test()
+        public void LoadTheData()
         {
             var sessionFactory = CreateSessionFactory();
-            var schemaExport = new SchemaExport(_configuration);
             using (var session = sessionFactory.OpenSession())
             {
-                schemaExport.Execute(true, true, false, session.Connection, null);
-                var customer = new Trade { Amount = 123 };
-                session.SaveOrUpdate(customer);
-
+                for (var i = 0; i < 100000; i++)
+                {
+                    var t = new Trade { Amount = i };
+                    var c = new Clearance { Amount = i };
+                    var m = new Match(t, c);
+                    session.SaveOrUpdate(m);
+                }
+                
                 session.Flush();
+            }
+        }
 
-                var foo = session.Query<Trade>().ToList();
-                Assert.AreEqual(foo.First().Amount, 123);
+        [Test]
+        public void SerializeTheResults()
+        {
+            
+            var sessionFactory = CreateSessionFactory();
+            using (var session = sessionFactory.OpenSession())
+            {
+                var stopwatch = Stopwatch.StartNew();
+                
+                var result = session.Query<Match>()
+                    .Fetch(x => x.Clearance)
+                    .Fetch(x => x.Trade).ToList();
+
+                //var serializedResult = JsonConvert.SerializeObject(result);
+                //Console.WriteLine("TotalSize: {0} MB", GetSize(serializedResult));
+
+                stopwatch.Stop();
+                Console.WriteLine("Elapsed: {0}", stopwatch.ElapsedMilliseconds);
+            }
+            
+        }
+
+        public decimal GetSize(object obj)
+        {
+            using (Stream s = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(s, obj);
+                return decimal.Round(s.Length / 1024.0m / 1024, 3);
+            }
+        }
+    }
+
+    [TestFixture]
+    public class DapperFixture
+    {
+        [Test]
+        public void ShouldDoSometingInteresting()
+        {
+            
+            using (IDbConnection conn = new SqlConnection(@"Server=TW-WINDOWS\SQLEXPRESS;Database=AggregatePatterns;Trusted_Connection=True;"))
+            {
+                var stopwatch = Stopwatch.StartNew();
+    
+                var result = conn.Query<Match, Trade, Clearance, Match>(
+                    "SELECT * FROM MATCH " +
+                    "INNER JOIN Trade ON Trade.Id = Match.Trade_Id " +
+                    "INNER JOIN Clearance ON Clearance.Id = Match.Trade_Id",
+                    (match, trade, clearance) =>
+                    {
+                        match.Trade = trade;
+                        match.Clearance = clearance;
+                        return match;
+                    });
+
+                //var serializedResult = JsonConvert.SerializeObject(result);
+                //Console.WriteLine("TotalSize: {0} MB", GetSize(serializedResult));
+
+                stopwatch.Stop();
+                Console.WriteLine("Elapsed: {0}", stopwatch.ElapsedMilliseconds);
+            }
+        }
+
+        public decimal GetSize(object obj)
+        {
+            using (Stream s = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(s, obj);
+                return decimal.Round(s.Length / 1024.0m / 1024, 3);
             }
         }
     }
@@ -77,8 +156,8 @@ namespace AggregatePatterns
         public MatchMap()
         {
             Id(x => x.Id);
-            References(x => x.Trade);
-            References(x => x.Clearance);
+            References(x => x.Trade).Cascade.SaveUpdate();
+            References(x => x.Clearance).Cascade.SaveUpdate();
         }
     }
 
